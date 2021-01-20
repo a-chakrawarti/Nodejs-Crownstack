fa0df4edf1d392e7b3ca5af2387de455d311003c
CURVX <chakrawarti.anupam@gmail.com>
2021-01-20 13:50:03 +0530

    update progress

--- source-code/miscellaneous/regex.js
+++ source-code/miscellaneous/regex.js
@@ -9,7 +9,7 @@ reg = /apple/i; // i means case insensitive flag
 
 log(reg.source); // this returns the value of regex literal
 
-let s = 'This is an apple is an apple that is an apple';
+let s = 'This is an apple is an apple that is an apple which costs 999';
 
 // Functions to match expressions
 
@@ -68,11 +68,68 @@ reg = /a*ple/; // matches any 0 or more characters
 
 reg = /ap?ples?/; // ? after character means that character is optional
 
-// character set 
+// character set [] : a-zA-z
+
+reg = /a[a-z]ple/;
+
+reg = /a[pzx]ple/; // can be a p, z, x
+
+reg = /a[^pzx]ple/; // cannot be p, z, x // output false
+
+reg = /a[a-zA-Z]p[labe][djje]/;
+
+
+// Quantifiers {}
+
+reg = /ap{2}le/; // p cab occur exactly 2 times
+
+reg = /ap{0,2}le/; // p can occur 0 or 1 or 2
+
+// Groupings : we use parantheses for groupings
+
+reg = /a(p){2}le/; // grouping and quantify
+
+reg = /a([prs]){2}le/;
+
+// character classes :
+
+reg = /\wle/; // word charcater _ or alphanumeric (one character)
+
+reg = /\w+e is/; // \w+ means one or more word characters
+
+reg = /\Wthis/; // Non word character
+
+reg = /\W+this/; // \W+ means more than one Non word character
+
+reg = /costs \d+/; // \d means digit and d+ means more than one digit
+
+// \D and \D+ are non digits and more than one non digit
+
+reg = /\sapple/; // \s matches whitespace characters (eg space, \t)
+
+// \s+ matches one or more than one whitespace characters
+
+reg = /\Sapple/; // match non whitespace characters
+
+reg = /\S+ple/; // match one or more than one non whitespace characters
+
+reg = /is\b/; // word boundary
+
+
+// Assertions
+
+reg = /a(?=p)/; // after a we need a character p
+
+reg = /a(?!p)/; // anything but p after a // "an"
+
+
+// Console-log outputs
 
 if (reg.test(s)) {
+    log(reg.exec(s));
     log('The string matches.');
 } else {
     log('The string doesn\'t matches.');
 }
 
+// Reference: https://www.youtube.com/watch?v=vyMlFKRfrNk
