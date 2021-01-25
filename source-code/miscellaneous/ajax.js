03724a551bec5a31a3d191fa9477dce3f141d788
CURVX <chakrawarti.anupam@gmail.com>
2021-01-25 13:56:38 +0530

    AJAX, GET & POST requests
    Made AJAX calls to GET data and show in index.html and POST data in JSON

--- source-code/miscellaneous/ajax.js
+++ source-code/miscellaneous/ajax.js
@@ -21,4 +21,73 @@
 // Saves network bandwidth
 // very interactive
 
+let fetchBtn = document.getElementById('fetchBtn');
+fetchBtn.addEventListener('click', buttonClickedHandler);
 
+function buttonClickedHandler() {
+    console.log('Fetch Button Clicked!');
+    // Instantiate an xhr object
+    const xhr = new XMLHttpRequest();
+
+    // Open the object
+    // xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/1', true); // true: async or not // non-blocking
+    // Post request
+    xhr.open('POST', 'http://dummy.restapiexample.com/api/v1/create', true);
+
+    xhr.getResponseHeader('Content-type', 'application/json');
+
+    xhr.onprogress = function () {
+        console.log('On progress!'); // Spinner animation
+    }
+
+    xhr.onreadystatechange = function () {
+        console.log('Ready state is: ', xhr.readyState);
+    }
+
+    xhr.onload = function () {
+        if (this.status === 200) {
+            console.log(this.responseText)
+        } else {
+            console.log('Some error occured!');
+        }
+
+    }
+    // send the request
+    data = { "name": "Rahul Sharma", "salary": "$73K", "age": "25" };
+    xhr.send(data);
+}
+
+console.log('Despite having this command after XHR, this runs before because "async" is set to TRUE!')
+
+// http://dummy.restapiexample.com/api/v1/employees
+
+let popBtn = document.getElementById('popBtn');
+popBtn.addEventListener('click', popHandler);
+
+function popHandler() {
+    console.log('Populate DOM Clicked!');
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'http://dummy.restapiexample.com/api/v1/employees', true);
+
+    xhr.onload = function () {
+        if (this.status === 200) {
+            let obj = JSON.parse(this.responseText);
+            console.log(obj);
+            let list = document.getElementById('list');
+            str = "";
+            let data = obj['data'];
+            for (idx in data) {
+                str += `<li>${data[idx].employee_name}</li>`;
+            }
+            list.innerHTML = str;
+        } else {
+            console.log('Some error occured!');
+        }
+    }
+
+    xhr.send();
+    console.log('Fetching employee data ...');
+
+}
