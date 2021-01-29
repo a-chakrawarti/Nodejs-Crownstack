8ae2a1924335605f1c3196680fd02222fcc382ba
CURVX <chakrawarti.anupam@gmail.com>
2021-01-29 12:18:58 +0530

    Added project/date-time
    Makes use of date object and setInterval to show real-time date and time

---
+++ project/date-time/date-time.js
@@ -0,0 +1,69 @@
+// adding html elements to show date and time
+let clockDiv = document.createElement('div');
+document.body.appendChild(clockDiv);
+clockDiv.setAttribute("id", "clock");
+let clockElement = document.getElementById('clock')
+
+
+// adding 'st', 'nd', 'rd', 'th' on dates
+function getDaySuffix(day) {
+    let daySuffix;
+    if (day == 1) {
+        daySuffix = "st"
+    } else if (day == 2) {
+        daySuffix == "nd"
+    } else if (day == 3) {
+        daySuffix == "rd"
+    } else {
+        daySuffix = "th"
+    }
+    return daySuffix;
+}
+
+
+function updateTime() {
+    var currentDate = new Date();
+    // day and date
+    var year = currentDate.getFullYear();
+    var day = currentDate.getDate();
+    var month = currentDate.getMonth()
+    var monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    var daySuffix = getDaySuffix(day);
+
+
+    // time 
+    var hours = currentDate.getHours();
+    var mins = currentDate.getMinutes();
+    var secs = currentDate.getSeconds();
+    var strToAppend;
+
+    if (hours > 12) {
+        strToAppend = "PM";
+        if (hours < 22)
+            hours1 = "0" + (hours - 12);
+        else
+            hours1 = String(hours);    
+    }
+    else if (hours < 12) {
+        strToAppend = "AM";
+        if (hours < 10)
+            hours1 = "0" + (hours);
+        else
+            hours1 = String(hours);   
+    }
+    else {
+        hours1 = hours;
+        strToAppend = "PM";
+    }
+    
+    // adding a "0" when value of minute is less than 10
+    if (mins < 10)
+        mins = "0" + mins;
+    if (secs < 10)
+        secs = "0" + secs;
+
+    clockElement.innerHTML = `Today is ${day}${daySuffix} ${monthName[month]}, ${year}</br>
+    <b>Current time is: </b>${hours1}:${mins}:${secs} ${strToAppend}`
+}
+
+setInterval(updateTime, 1000);
